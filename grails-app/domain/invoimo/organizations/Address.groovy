3994db514b37c2a810df920800daff1a5bd10fa3
Philipp von Böselager <philipp.boeselager@web.de>
2017-08-24 17:26:59 +0200

    Add simple content lines to Address

--- grails-app/domain/invoimo/organizations/Address.groovy
+++ grails-app/domain/invoimo/organizations/Address.groovy
@@ -2,6 +2,11 @@ package invoimo.organizations
 
 class Address {
 
+    String line1
+    String line2
+    String line3
+
     static constraints = {
+        line1(blank:false, size:20..100)
     }
 }
